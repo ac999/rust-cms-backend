5b7efb1c5ef6f7c4c0d8fdc99b68fc236a25d739
Andrei C <andreicristian6@protonmail.com>
2019-07-16 11:26:28 +0300

    removed target

--- server/target/debug/deps/server-77248b46060e2e20.d
+++
@@ -1,5 +0,0 @@
-/home/m0ric/Projects/zoolx/rust-backend/server/target/debug/deps/server-77248b46060e2e20: src/main.rs
-
-/home/m0ric/Projects/zoolx/rust-backend/server/target/debug/deps/server-77248b46060e2e20.d: src/main.rs
-
-src/main.rs:
--- server/target/debug/server.d
+++
@@ -1,1 +0,0 @@
-/home/m0ric/Projects/zoolx/rust-backend/server/target/debug/server: /home/m0ric/Projects/zoolx/rust-backend/server/src/main.rs
