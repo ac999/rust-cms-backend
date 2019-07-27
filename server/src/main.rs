eb0d88d5d74bea1558a8bb3f90868472681c83c4
Marius C <mfc@marius-cristian.com>
2019-07-27 14:19:30 +0200

    config File object structure

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,22 +1,25 @@
-use mongodb::{Bson, bson, doc};
-use mongodb::{Client, ClientOptions, ThreadedClient};
-use mongodb::db::ThreadedDatabase;
-pub use crate::db_connection::connection;
+// use mongodb::{Bson, bson, doc};
+// use mongodb::{Client, ClientOptions, ThreadedClient};
+// use mongodb::db::ThreadedDatabase;
+// pub use crate::db_connection::connection;
 
-fn main() {
-    let client = connection::db_client("127.0.0.1", 27017,
-                                       connection::set_ssl_options(
-            "ssl/ca.crt", "ssl/mongodb.crt", "ssl/mongodb.key", false)
-        );
+// fn main() {
+//     let client = connection::db_client("127.0.0.1", 27017,
+//                                        connection::set_ssl_options(
+//             "ssl/ca.crt", "ssl/mongodb.crt", "ssl/mongodb.key", false)
+//         );
 
-    let coll = client.db("zoolx").collection("users");
+//     let coll = client.db("zoolx").collection("users");
 
-    let doc = doc! {
-        "_id": "test"
-    };
+//     let doc = doc! {
+//         "_id": "test"
+//     };
 
-    let mut cursor = coll.find(Some(doc.clone()), None)
-        .ok().expect("Failed to execute find.");
+//     let mut cursor = coll.find(Some(doc.clone()), None)
+//         .ok().expect("Failed to execute find.");
 
-    let item = cursor.next();
+//     let item = cursor.next();
+// }
+fn main(){
+	
 }
