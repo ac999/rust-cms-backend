786c6240ec8f92c9b8e5b23322e0a881fb0ccdfb
Andrei C <andreicristian6@protonmail.com>
2019-08-07 18:10:51 +0300

    reading the config from .env works

--- src/config.rs
+++ src/config.rs
@@ -37,17 +37,17 @@ pub fn read_env() -> ConfigStruct{
 }
 
 pub fn get_db_url(config: ConfigStruct) -> String {
-	String::new()
+	config.db_url
 }
 
 pub fn get_ca(config: ConfigStruct) -> String {
-	String::new()
+	config.ca_path
 }
 
 pub fn get_crt(config: ConfigStruct) -> String{
-	String::new()
+	config.crt_path
 }
 
 pub fn get_key(config: ConfigStruct) -> String{
-	String::new()
+	config.key_path
 }
